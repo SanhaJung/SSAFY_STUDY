7e3a54a1e7c37e525d7fcc69438496bfe8ce1ab0
용한 <byh5112@naver.com>
2021-09-25 15:46:31 +0900

    푸쉬

---
+++ 0915/yonghan/Main_bj_15685_드래곤커브.java
@@ -0,0 +1,55 @@
+package a0915;
+
+import java.io.*;
+import java.util.*;
+
+public class Main_bj_15685_드래곤커브 {
+	static boolean[][] map = new boolean[101][101];
+	static int[] dx = { 1, 0, -1, 0 };
+	static int[] dy = { 0, -1, 0, 1 };
+	static int answer = 0;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			int g = Integer.parseInt(st.nextToken());
+
+			dragonCurve(x, y, d, g);
+		}
+
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (map[i][j] && map[i][j + 1] && map[i + 1][j] && map[i + 1][j + 1]) {
+					answer++;
+				}
+			}
+		}
+
+		System.out.println(answer);
+		br.close();
+	}
+
+	public static void dragonCurve(int x, int y, int d, int g) {
+		List<Integer> list = new ArrayList<>();
+		list.add(d);
+
+		for (int i = 1; i <= g; i++) {
+			for (int j = list.size() - 1; j >= 0; j--) {
+				list.add((list.get(j) + 1) % 4);
+			}
+		}
+
+		map[y][x] = true;
+		for (Integer direction : list) {
+			x += dx[direction];
+			y += dy[direction];
+			map[y][x] = true;
+		}
+	}
+}
