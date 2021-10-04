497ed51a9b4b4fb642de7493759b4eec0534ebbb
yminsang96 <yminsang96@gmail.com>
2021-10-05 00:23:50 +0900

    refactoring

--- 1003/yms/Main_bj_17142_연구소3.java
+++ 1003/yms/Main_bj_17142_연구소3.java
@@ -1,125 +1,99 @@
 package bj17142;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-public class Main_bj_17142_연구소3 {
-	static int N, M;
-	static int[] numbers; // 뽑은 조합을 기록
-	static ArrayList<Virus> virus = new ArrayList<>();
-	
+public class Main_bj_17142_연구소3 {	
+	static int N,M,blank,time;
+	static int[] dr = {-1,0,1,0}, dc = {0,1,0,-1};
 	static int[][] map;
-	static boolean[][] visited;
-	static int blank;		// 빈칸 기록
+	static ArrayDeque<int[]> q = new ArrayDeque<>();
+	static ArrayList<int[]> virus = new ArrayList<>();
+	static int answer = Integer.MAX_VALUE/2;
+	static int[] numbers;
+	static boolean[][] visited = new boolean[N][N];
 	
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,1,0,-1};
-	
-	static int min = Integer.MAX_VALUE/2;
-	
-	static class Virus {
-		int r;
-		int c;
-		int time;
-		
-		public Virus(int r, int c, int time) {
-			this.r = r;
-			this.c = c;
-			this.time = time;
-		}
-	}
-	
-	static void combination(int cnt, int start) {
-		if(cnt == M) {
-			bfs();
+	static void combination(int start, int cnt) {
+		if(cnt==M) {
+			q.clear();
+			visited =new boolean[N][N];
+			for(int i=0; i<numbers.length; i++) {
+				int[] cur = virus.get(numbers[i]);
+				visited[cur[0]][cur[1]] = true;
+				q.offer(new int[] {cur[0], cur[1]});
+			}
+			if(bfs()) {
+				answer = Math.min(answer, time);
+			}
 			return;
 		}
 		
-		for (int i = start; i < virus.size(); i++) {
+		for(int i=start; i<virus.size(); i++) {
 			numbers[cnt] = i;
-			combination(cnt+1, i+1);
+			combination(i+1,cnt+1);
 		}
 	}
-
-	static void bfs() {
-		ArrayDeque<Virus> q = new ArrayDeque<>();
-		visited = new boolean[N][N];
-		
-		for (int i = 0; i < M; i++) {
-			q.add(virus.get(numbers[i]));
-			visited[virus.get(numbers[i]).r][virus.get(numbers[i]).c] = true;
-		}
-		
-		int time = 0;
-		int count = 0;		// 몇 칸을 확산 했는지 기록할 변수
+	
+	static boolean bfs() {		
+		int cnt = 0;
+		time = 0;
 		
 		while(!q.isEmpty()) {
-			Virus virus = q.poll();
-			for (int d = 0; d < 4; d++) {
-				int nr = virus.r + dr[d];
-				int nc = virus.c + dc[d];
-			
-				time = virus.time;
-				
-				if(0<=nr && nr<N && 0<=nc && nc<N && !visited[nr][nc]) {
-					if(map[nr][nc] == 0) {										// 빈칸이라면
+			int size = q.size();
+			for(int i=0; i<size; i++) {
+				int[] cur = q.poll();
+				int r = cur[0];
+				int c = cur[1];
+				for(int d=0; d<4; d++) {
+					int nr = r+dr[d];
+					int nc = c+dc[d];
+					if(0<=nr && nr<N && 0<=nc && nc<N && !visited[nr][nc] && map[nr][nc]!=1) {
 						visited[nr][nc] = true;
-						count++;
-						q.add(new Virus(nr,nc,virus.time+1));
-					}
-					if(map[nr][nc] == 2) {										// 비활성바이러스라면
-						visited[nr][nc] = true;
-						q.add(new Virus(nr,nc,virus.time+1));
+						q.offer(new int[] {nr,nc});
+						if(map[nr][nc]==0) cnt++;
 					}
 				}
+				if(cnt == blank) {
+					time++;
+					return true;
+				}
 			}
-			
-			if(count == blank) {
-				time++;	// 이경우, 새로 뽑은 시간값 할당이 안되므로, 임의로 1 증가
-				break;
-			}
-		};
+			time++;
+		}		
+		return false;
+	}
 	
-		if(count != blank) return;	// 다 못채우면 그냥 끝내기
-		
-		// time-1의 이유 : 마지막에 +1한 상태를 q에 날리기 때문
-		min = Math.min(min, time-1);
-	}	
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken()); // 연구소 크기
-		M = Integer.parseInt(st.nextToken()); // 바이러스 개수
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[N][N];
+		numbers = new int[M];
 		
-		for (int i = 0; i < N; i++) {
+		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < N; j++) {
+			for(int j=0; j<N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				if(map[i][j] == 2) {
-					virus.add(new Virus(i,j,1));
-				}else if(map[i][j] == 0) {
+				if(map[i][j] == 0) {
 					blank++;
+				}
+				else if (map[i][j] == 2) {
+					virus.add(new int[] {i,j});
 				}
 			}
 		}
 		
-		if(blank == 0) {
-			System.out.println(0);
-			return;
+		if(blank == 0)
+			answer = 0;
+		else {
+			combination(0,0);
+			if(answer == Integer.MAX_VALUE/2)
+				answer = -1;
 		}
+		System.out.println(answer);
 		
-		numbers = new int[M];
-		
-		combination(0, 0);
-		
-		if(min == Integer.MAX_VALUE/2) {
-			System.out.println(-1);
-			return;
-		}
-		
-		System.out.println(min);
 	}
 }
