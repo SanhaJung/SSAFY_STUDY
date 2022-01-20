d1ae9672c12cbe2ff9621e9ef9738c0b2d60ca89
yminsang96 <yminsang96@gmail.com>
2022-01-20 22:05:44 +0900

    0120

---
+++ 220120/yms/Solution_pr_위장.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+            String s = clothes[i][1];
+            if(!map.containsKey(s))  map.put(s,1);
+            else map.put(s,map.get(s)+1);
+        }
+        
+        for(String s : map.keySet()) { 
+            answer *= (map.get(s)+1); 	// map.get(s) + 아무것도 안고르는 경우의 수 1
+        }
+
+        return answer-1;	// 아무것도 안고르는 경우 1 빼줌
+    }
+}
